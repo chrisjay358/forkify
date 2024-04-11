0e9fa350c7ba8213d3934ec3b5eb2dcb53155228
chrisjay358 <chrisjay358@icloud.com>
2024-04-11 18:22:58 +0100

    Fixture

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,6 +7,9 @@ import paginationView from "./views/paginationView.js";
 import bookmarksView from "./views/bookmarksView.js";
 import addRecipeView from "./views/addRecipeView.js";
 
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 // if (module.hot) {
 //   module.hot.accept();
 // }
