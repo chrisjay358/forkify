ffe93e027dbabe94aa2a63df9bbd24f3c4fe790d
chrisjay358 <chrisjay358@icloud.com>
2024-04-11 16:22:56 +0100

    New welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -132,6 +132,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
+  console.log("Welcome");
 };
 
 init();
